36f45beaa112b540e71c9b437691c2b00673c70f
Sarmad Raees Khan <raeessarmad200@gmail.com>
2024-04-15 11:29:01 +0200

    add review, special offer and services

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,21 +1,30 @@
-const handleClick = () => {
-  console.log("Button Clicked")
-}
-const Button = ({ label, iconUrl }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
   return (
     <button
-      className=" bg-coral-red border-coral-red text-white px-7 py-4 rounded-full justify-center items-center gap-2 flex flex-row text-lg leading-none"
-      onClick={handleClick}
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white border-coral-red"
+        } rounded-full ${fullWidth && "w-full"}`}
     >
       {label}
-      <img
-        src={iconUrl}
-        height={15}
-        width={15}
-        className="ml-2 rounded-full w-5 h-5"
-      />
-    </button>
-  )
-}
 
-export default Button
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt='arrow right icon'
+          className='ml-2 rounded-full bg-white w-5 h-5'
+        />
+      )}
+    </button>
+  );
+};
+
+export default Button;
